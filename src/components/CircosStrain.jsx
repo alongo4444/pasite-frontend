3e6ed3cbb74f93a4a53a05ab826e391684b9cd57
alongolo <alongolo@post.bgu.ac.il>
2021-03-24 18:20:30 +0200

    Alon Golombek - 24.3.21
    Added to the DownloadPage.jsx a feature to download genes of a selected defense system/s.

--- src/components/CircosStrain.jsx
+++ src/components/CircosStrain.jsx
@@ -1,11 +1,12 @@
-import React, {Component, useEffect, useState} from "react";
+import React, {Component} from "react";
 import axios from "axios";
+import InnerHTML from 'dangerously-set-html-content'
 
 class CircosStrain extends Component {
     state = {
         file: null,
         loaded: false,
-        scriptTxt: ""
+        head_scripts: null
     };
 
 
@@ -16,104 +17,37 @@ class CircosStrain extends Component {
             )
             .then(response => {
                 this.setState({file: response.data});
-                // this.setState({file: response.data});
-
                 this.setState({loaded: true})
-
-                let extractscript = /<script(.+)<\/script>/gi.exec(this.state.file);
-                this.setState({file: this.state.file.replace(extractscript[0], "")});
-                this.setState({scriptTxt: JSON.stringify(extractscript[1])})
-                window.eval(extractscript[1]);
-
-                console.log('a')
-
             });
 
-        // const script = document.getElementById('circost').innerHTML;
-        // window.eval(script);
+        axios
+            .get(
+                "http://127.0.0.1:8801/api/v1/strains/strainCircosScripts/" + this.props.svnn,
+            )
+            .then(response => {
+                this.setState({head_scripts: response.data.head});
+                let arr = this.state.head_scripts;
+                arr.forEach(function(entry) {
+                    window.eval(entry)
 
+                    // let script= document.createElement('script');
+                    // script.text = entry;
+                    // document.head.appendChild(script);
 
+                });
+
+            });
     }
 
     render() {
-        return (<iframe dangerouslySetInnerHTML={{ __html: this.state.file}}/>);
+        // return (<iframe dangerouslySetInnerHTML={{ __html: this.state.file}}/>);
 
         return (
-            <div>
-                {/*{renderHTML(htmlString)}*/}
-                {/*<div dangerouslySetInnerHTML={{__html: /'<iframe src=`${this.}'/>*/}
-                {/*<script data-for="htmlwidget-7224326d117abd6ef328" className='structured-data-list'*/}
-                {/*        type="application/json" dangerouslySetInnerHTML={ { __html: this.state.scriptTxt}} />*/}
-                {/*//   <InnerHTML html={this.state.file} />*/}
-            </div>
+            // <div dangerouslySetInnerHTML={{ __html: this.state.file }}/>
+            <InnerHTML html={this.state.file} />
         )
-
     }
 
 }
 
 export default CircosStrain;
-
-// export default function CircosStrain({svnn}) {
-//     const [file, setFile] = useState(0);
-//     const [loaded, setLoaded] = useState(false);
-//     // Similar to componentDidMount and componentDidUpdate:
-//     useEffect(() => {
-//         // Update the document title using the browser API
-//         axios
-//             .get(
-//                 "http://127.0.0.1:8801/api/v1/strains/strainCircos/" + svnn,
-//             )
-//             .then(response => {
-//                 // this.setState({file: response.data});
-//                 // this.setState({loaded: true})
-//                 setFile(response.data)
-//                 setLoaded(true)
-//             });
-//     });
-//
-//     return(
-//         <div>
-//             <InnerHTML html={file} />
-//         </div>
-//     )
-// }
-
-// export default class CircosStrain extends React.Component {
-//
-//         state = {
-//         file: null,
-//         loaded: false
-//     };
-//
-//     constructor(props) {
-//         super(props);
-//     }
-//
-//     componentDidMount() {
-//
-//                 axios
-//             .get(
-//                 "http://127.0.0.1:8801/api/v1/strains/strainCircos/" + this.props.svnn,
-//             )
-//             .then(response => {
-//                 this.setState({file: response.data});
-//                 this.setState({loaded: true})
-//             });
-//
-//
-//         const s = document.createElement('script');
-//         s.type = 'text/javascript';
-//         s.async = true;
-//         s.innerHTML = "document.write(alert('test'))";
-//         this.instance.appendChild(s);
-//     }
-//
-//     render() {
-//         return (
-//             <div>
-//                 <div ref={el => (this.instance = el)} />
-//             </div>
-//         );
-//     }
-// }
