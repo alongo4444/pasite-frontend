dcf2cb4f3dcdae31c0957f8f3532042fec12b2f5
yinonbz <yinonbz@gmail.com>
2021-03-26 04:20:32 +0300

    design main page and browse

--- src/pages/BrowsePage.jsx
+++ src/pages/BrowsePage.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component,useEffect, useState} from "react";
 import FadeIn from "react-fade-in";
 import axios from "axios";
 import {TransformWrapper, TransformComponent} from "react-zoom-pan-pinch";
@@ -14,6 +14,7 @@ import Button from 'react-bootstrap/Button';
 import '../assets/fonts/YesevaOne-Regular.ttf';
 import Switch from "react-switch";
 import AutocompleteC from "../components/AutocompleteC";
+import MiniDrawer from "../components/Drawer";
 
 var qs = require('qs');
 
@@ -27,10 +28,14 @@ class BrowsePage extends Component {
             textOrFile: 'Text Box',
             selectedOption: [],
             selectedFile: {},
-            selectedStrains: []
+            selectedStrains: [],
+            isOpen:false
         }
     };
 
+    /*
+    load empty phylogenetic tree as default tree
+     */
     componentDidMount() {
         axios
             .get(
@@ -49,15 +54,20 @@ class BrowsePage extends Component {
             });
     }
 
+    /*
+    compute a new tree when the user click the button "generate tree"
+    similar to the orginial function, this time - with query params.
+     */
     computeTree = () => {
         this.setState({source: []});
-        this.setState({loaded: false})
+        this.setState({loaded: false});
+        let systems =[]
         return axios
             .get(
                 "http://127.0.0.1:8800/api/v1/strains/phyloTree", {
                     params: {
                         systems: this.state.selectedOption.map((option) => option.label),
-                        subtree: this.state.selectedFile
+                        subtree: this.state.selectedFile.length >0 ? this.state.selectedFile: this.state.selectedStrains
                     },
                     paramsSerializer: function (params) {
                         return qs.stringify(params, {arrayFormat: 'repeat'})
@@ -75,9 +85,15 @@ class BrowsePage extends Component {
                 this.setState({source: "data:;base64," + base64});
                 this.setState({loaded: true})
                 this.setState({selectedFile: {}})
+                this.setState({selectedOption: []})
             }).catch((err) => console.log(err)
             );
     };
+    /*
+    handle file upload and load each line to array of
+     integers (aka strain indexes for subtree) for subtree generating
+     using selectedFile state.
+     */
     onFileChange = e => {
 
         // Update the state
@@ -95,28 +111,42 @@ class BrowsePage extends Component {
 
     };
 
+    /*
+    handle the values inserted to autocomplete component and
+    saves them in variable of subtree selectedStrains
+     */
+
     handleTextBox = selected => {
 
         // Update the state
         if (selected.length > 0) {
             let array = [];
-
             Object.keys(selected).map((key, index) => (
-                array.push(selected[key]['name'])
+                array.push(selected[key]['index'])
             ))
-            this.setState({selectedStrains: array});
+            this.setState({selectedStrains: [...array]});
         }
 
     };
 
 
     render() {
+        /*
+        handles defense systems choice into selectedOptions state and save it.
+         */
         const handleChange = selectedOption => {
+            if(selectedOption == null){
+                selectedOption = []
+            }
             this.setState(
                 {selectedOption},
                 () => console.log(`Option selected:`, this.state.selectedOption)
             );
         };
+        /*
+        color the defense systems options in the autocomplete box.
+        also, handles multi value selection in it.
+         */
         const colourStyles = {
             control: styles => ({...styles, backgroundColor: 'white'}),
             option: (styles, {data, isDisabled, isFocused, isSelected}) => {
@@ -166,11 +196,15 @@ class BrowsePage extends Component {
                 },
             }),
         };
+
+        /*
+        render the div of file upload or strain selection - according the
+         choice of the user (using switch).
+         */
         const renderTextBox = () => {
             if (this.state.textbox == true) {
-                return <AutocompleteC multipleChoice={true} apiUrl="http://127.0.0.1:8800/api/v1/strains"
-                                      labelText=""
-                                      parentCallback={this.handleTextBox}/>;
+                 return <AutocompleteC  multipleChoice={true} apiUrl="http://127.0.0.1:8800/api/v1/strains/indexes"
+                                      parentCallback={this.handleTextBox}/>
             } else {
                 return <Form.Group>
                     <Form.File onChange={(e) => this.onFileChange(e)} id="exampleFormControlFile1"
@@ -179,6 +213,9 @@ class BrowsePage extends Component {
             }
         }
 
+        /*
+        update the state of the file upload/strain selection on change
+         */
         const setSwitchTextBox = () => {
             if (this.state.textbox == true) {
                 this.setState({textbox: false});
@@ -189,7 +226,6 @@ class BrowsePage extends Component {
                 this.setState({textOrFile: 'Text Box'});
             }
         }
-
         return (
             <div className="mainDiv">
                 <FadeIn>
@@ -229,7 +265,7 @@ class BrowsePage extends Component {
                             >
                                 {({zoomIn, zoomOut, resetTransform, ...rest}) => (
                                     <React.Fragment>
-                                        <div style={{marginLeft: "42%"}} className="tools">
+                                        <div style={{marginLeft: "40%"}} className="tools">
                                             <IconButton onClick={zoomIn} color="primary" aria-label="upload picture"
                                                         component="span">
                                                 <ZoomIn/>
@@ -260,6 +296,9 @@ class BrowsePage extends Component {
                                     </React.Fragment>
                                 )}
                             </TransformWrapper>
+                            <div id="drawer">
+                                <MiniDrawer />
+                            </div>
                         </div>
                     </div>
 
